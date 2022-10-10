22f870d44fcd72803777a3622b315d983092fc9a
f4ejkai <40180903+f4ejkai@users.noreply.github.com>
2022-10-10 13:26:27 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function show_now() {
+var my_time = new Date();
+document.getElementById('display').innerHTML=my_time
+}
+
+function print_page(){
+    print()
+}
+
+function when_birthday(){
+    document.write("My birthday is Sep,22nd,1995")
+}
